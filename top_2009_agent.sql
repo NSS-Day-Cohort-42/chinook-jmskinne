421026a648ad8aa8469715637163860b7736e942
Mark Skinner <jmskinne@gmail.com>
2020-11-23 14:58:04 -0600

    done

--- top_2009_agent.sql
+++ top_2009_agent.sql
@@ -1,16 +1,18 @@
-SELECT 
-    emp.EmployeeId as ID,
-    emp.FirstName as EmployeeFirstName,
-    emp.LastName as EmployeeLastName,
+SELECT
+    MAX(AgentSales) as TopAgentSales,
+    TopAgent2009
+FROM
     (
-        SELECT ROUND(SUM(inv.Total), 2)
-        FROM Invoice as inv
-        JOIN Customer as c ON c.SupportRepId = emp.EmployeeId
-        WHERE inv.CustomerId = c.CustomerId AND inv.InvoiceDate LIKE '2009%'
+        SELECT 
+            emp.FirstName TopAgent2009,
+            ROUND(SUM(inv.Total), 2) AgentSales
+        FROM Employee emp
+        JOIN Customer c ON c.SupportRepId = emp.EmployeeId
+        JOIN Invoice inv ON inv.CustomerId = c.CustomerId
+        WHERE inv.InvoiceDate LIKE '2009%'
+        GROUP BY TopAgent2009
+    );
 
-    ) as TotalAgentSales
-FROM Employee as emp
-WHERE TotalAgentSales > 1;
 
--- needs to return single employee
+
 
